e3ce4521554f1c78355db70564c4dfed17835858
Vincent Pham <vincent.pham013@gmail.com>
2021-01-26 14:18:08 +0700

    Show userinfo modal

--- src/layouts/User.tsx
+++ src/layouts/User.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 // reactstrap components
-import { Container } from 'reactstrap';
+import { Col, Container, FormGroup, Input, Row } from 'reactstrap';
 // core components
 import UserNavbar from '@/components/Navbars/UserNavbar';
 import UserFooter from '@/components/Footers/UserFooter';
 import SideBar from '@/components/Sidebar/Sidebar';
 import { authSelector, refreshTokenAsyncThunk } from '@/redux/features/auth';
 import { useDispatch, useSelector } from 'react-redux';
+import CustomModal from '@/components/Modal/Modal';
+import { updateUserInfoAsyncThunk } from '@/redux/features/user/user.thunk';
+import { userSelector } from '@/redux/features/user/user.slice';
 
 const routes = [
   {
@@ -41,12 +44,13 @@ function User(props) {
   const router = useRouter();
   const dispatch = useDispatch();
   const auth = useSelector(authSelector);
+  const userSl = useSelector(userSelector);
 
-  React.useEffect(() => {
-    document.documentElement.scrollTop = 0;
-    document.scrollingElement.scrollTop = 0;
-    // mainContentRef.current.scrollTop = 0;
-  }, []);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [job, setJob] = useState('');
 
   const getBrandText = () => {
     for (let i = 0; i < routes.length; i++) {
@@ -57,6 +61,54 @@ function User(props) {
     return 'Brand';
   };
 
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const onPhoneChange = (e) => {
+    setPhone(e.target.value);
+  };
+
+  const onJobChange = (e) => {
+    setJob(e.target.value);
+  };
+
+  const onModalSubmit = () => {
+    if (!phone || !job || !email) {
+      alert('Vui lòng nhập đầy đủ thông tin');
+      return;
+    }
+
+    dispatch(
+      updateUserInfoAsyncThunk({
+        id: userSl.id,
+        name: userSl.name,
+        email,
+        phone,
+        job,
+      }),
+    );
+  };
+
+  useEffect(() => {
+    if (userSl.status === 'succeeded') {
+      if (!userSl.email || !userSl.phone || !userSl.job) {
+        setIsOpenModal(true);
+      } else {
+        setIsOpenModal(false);
+      }
+      
+      setName(userSl.name);
+      setEmail(userSl.email);
+      setJob(userSl.job);
+      setPhone(userSl.phone);
+    }
+  }, [userSl]);
+
   useEffect(() => {
     if (!auth.isAuthenticated && !auth.token) {
       dispatch(refreshTokenAsyncThunk());
@@ -66,7 +118,7 @@ function User(props) {
   // check authentication
   useEffect(() => {
     if (
-      (auth.status === 'failed' && !auth.token)||
+      (auth.status === 'failed' && !auth.token) ||
       (auth.status === 'reset' && !auth.token)
     ) {
       router.push('/auth/login');
@@ -83,7 +135,11 @@ function User(props) {
     }
   }, [auth]);
 
-  if (auth.status === 'failed' || auth.status === 'loading' || auth.user?.roleId !== 1) {
+  if (
+    auth.status === 'failed' ||
+    auth.status === 'loading' ||
+    auth.user?.roleId !== 1
+  ) {
     return <div>Đang tải trang</div>;
   }
 
@@ -105,6 +161,58 @@ function User(props) {
           <UserFooter />
         </Container>
       </div>
+      <CustomModal
+        titleHeader="Vui hoàn tất các thông tin sau"
+        isOpen={isOpenModal}
+        onSubmit={onModalSubmit}>
+        <Row>
+          <Col lg="6">
+            <FormGroup>
+              <label className="form-control-label" htmlFor="input-email">
+                Email
+              </label>
+              <Input
+                className="form-control-alternative"
+                value={email}
+                id="input-email"
+                onChange={onEmailChange}
+                placeholder="Email"
+                type="text"
+              />
+            </FormGroup>
+          </Col>
+          <Col lg="6">
+            <FormGroup>
+              <label className="form-control-label" htmlFor="input-phone">
+                Số điện thoại
+              </label>
+              <Input
+                className="form-control-alternative"
+                value={phone}
+                id="input-phone"
+                onChange={onPhoneChange}
+                placeholder="Số điện thoại"
+                type="text"
+              />
+            </FormGroup>
+          </Col>
+          <Col lg="6">
+            <FormGroup>
+              <label className="form-control-label" htmlFor="input-job">
+                Công việc
+              </label>
+              <Input
+                className="form-control-alternative"
+                value={job}
+                id="input-job"
+                onChange={onJobChange}
+                placeholder="Công việc"
+                type="text"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
+      </CustomModal>
     </>
   );
 }
