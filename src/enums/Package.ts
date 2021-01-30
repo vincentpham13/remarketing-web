93bb63c1757834667338abe9d43cf308c3516843
Cuong Nguyen <cuongdollar2995@gmail.com>
2021-01-30 11:09:57 +0700

    Finish implement (#18)
    Co-authored-by: CuongNV <cuongnguyen2995@gmail.com>

---
+++ src/enums/Package.ts
@@ -0,0 +1,9 @@
+export enum PackageType{
+    TimeAndMessage = 1,
+    MessageOnly = 2,
+}
+
+export enum PackageTypeLabel{
+    TimeAndMessage = 'Duy trì dịch vụ',
+    MessageOnly = 'Số lượng tin nhắn',
+}
