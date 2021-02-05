238373cd71943cdcf63be84445d9038582291dec
Vincent Pham <vincent.pham013@gmail.com>
2021-02-05 22:34:36 +0700

    Modify generic entity state

--- src/redux/interfaces/index.ts
+++ src/redux/interfaces/index.ts
@@ -1,4 +1,4 @@
 export interface IGenericEntityState {
-  status: 'idle' | 'loading' | 'succeeded' | 'failed' | 'reset';
+  status: 'idle' | 'loading' | 'succeeded' | 'creating-child' | 'creating-child-failed' | 'creating-child-succeeded' | 'loading-child' | 'failed' | 'reset';
   error: string | null;
 }
