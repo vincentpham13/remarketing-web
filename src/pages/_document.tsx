056fe31c56ab609d7046de744bf5642e34eb5d22
Vincent Pham <vincent.pham013@gmail.com>
2021-02-05 22:37:00 +0700

    embed fb messenger

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,11 +16,46 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          {/* <!-- Load Facebook SDK for JavaScript --> */}
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `window.fbAsyncInit = function() {
+                FB.init({
+                  xfbml            : true,
+                  version          : 'v9.0'
+                });
+              };
+      
+              (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));`,
+            }}
+          />
+          <script
+            async
+            defer
+            crossOrigin="anonymous"
+            src="https://connect.facebook.net/en_US/sdk.js"></script>
+          {/* <!-- Load Facebook SDK for JavaScript --> */}
+        </Head>
         <body>
           <div id="page-transition"></div>
+          <div id="fb-root"></div>
           <Main />
           <NextScript />
+          {/* <!-- Your Chat Plugin code --> */}
+          <div
+            class="fb-customerchat"
+            attribution="setup_tool"
+            page_id="103634791744953"
+            logged_in_greeting="Bombot hân hạnh được phục vụ quý khách!"
+            logged_out_greeting="Bombot hân hạnh được phục vụ quý khách!"></div>
         </body>
       </Html>
     );
